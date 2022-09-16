47411142d04687a26d18e9a1adf6813cc30f2d18
ParedesEzequiel <113204010+ParedesEzequiel@users.noreply.github.com>
2022-09-16 13:41:16 -0300

    Add files via upload

---
+++ Ejercicio01/Ejercicio01/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio01
+{
+    class Cuenta
+    {
+        private string titular;
+        private double cantidad;
+        private double cantidadRetiro;
+
+        public Cuenta(string titular)
+        {
+            this.titular = titular;
+        }
+
+        public Cuenta(string titular, double cantidad)
+        {
+            this.titular = titular;
+            this.cantidad = cantidad;
+        }
+
+        public void ingresar(float e)
+        {
+
+            if (e >= 0)
+            {
+                cantidad += e;
+            }
+        }
+
+        public void retirar(float f)
+        {
+            cantidadRetiro = cantidad - f;
+            if (cantidadRetiro < 0)
+            {
+                cantidad = 0;
+                cantidadRetiro = 0;
+            }
+        }
+        public string Titular
+        {
+            get
+            {
+                return titular;
+            }
+            set
+            {
+                titular = value;
+            }
+        }
+
+        public double Cantidad
+        {
+            set
+            {
+                cantidad = value;
+            }
+            get
+            {
+                return cantidad;
+            }
+        }
+
+        public double CantidadRetiro
+        {
+            set
+            {
+                cantidadRetiro = value;
+            }
+            get
+            {
+                return cantidadRetiro;
+            }
+        }
+
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Cuenta cuenta1 = new Cuenta("elpepe", 250.03);
+
+            cuenta1.retirar(300);
+
+            Console.WriteLine(cuenta1.Titular + " " + cuenta1.Cantidad);
+
+            cuenta1.ingresar(400);
+            cuenta1.ingresar(-200);
+
+            Console.WriteLine(cuenta1.Titular + " " + cuenta1.Cantidad);
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
+
